cb87bf0cfdb209cbba18b3d840c9743656b09ef9
Julien C <64107022+Julien-cpsn@users.noreply.github.com>
2022-11-07 00:58:07 +0100

    Add files via upload

---
+++ saver-sync.bat
@@ -0,0 +1,11 @@
+@echo off
+set "isaacPath=%cd%"
+cd /d %1
+git pull
+cd /d %isaacPath%
+start /wait isaac-ng.exe
+cd /d %1
+set "currentTime=%time%"
+git add -A
+git commit -m "%currentTime%"
+git push
